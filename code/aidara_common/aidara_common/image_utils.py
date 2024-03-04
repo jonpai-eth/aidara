27280b8873919976fc9ab90199816c1a1ced59e0
Artur von Ruffer <146971745+avonruffer@users.noreply.github.com>
2024-03-04 15:06:12 +0100

    Rewrite image_utils to avoid duplicate code. (#32)
    Co-authored-by: jonpai-eth <146820382+jonpai-eth@users.noreply.github.com>

--- code/aidara_common/aidara_common/image_utils.py
+++ code/aidara_common/aidara_common/image_utils.py
@@ -1,13 +1,9 @@
 """Utility functions for working with ZED cameras and its images."""
+from typing import TypeVar
+
 import numpy as np
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.node import Node
-from rclpy.qos import (
-    QoSDurabilityPolicy,
-    QoSHistoryPolicy,
-    QoSProfile,
-    QoSReliabilityPolicy,
-)
 from sensor_msgs.msg import CameraInfo, Image
 
 
@@ -22,36 +18,16 @@ def get_img_from_zed(node: Node, callback_group: ReentrantCallbackGroup) -> Imag
         callback_group: Reentrant callback group for the subscriber.
 
     Returns:
-        Latest image from ZED camera
+        Latest image from ZED camera.
     """
-    video_qos = QoSProfile(
-        depth=1,
-        history=QoSHistoryPolicy.KEEP_LAST,
-        reliability=QoSReliabilityPolicy.BEST_EFFORT,
-        durability=QoSDurabilityPolicy.VOLATILE,
-    )
-    img = None
-
-    def update_img(msg: Image) -> None:
-        nonlocal img
-        img = msg
-        node.logger.info("Received image.")
-        node.destroy_subscription(subs)
-
-    subs = node.create_subscription(
-        Image,
-        "/zed/zed_node/left/image_rect_color",
-        update_img,
-        qos_profile=video_qos,
-        callback_group=callback_group,
-    )
-
-    # Sleep until image can be retrieved to ensure image is there
     node.logger.info("Waiting for image...")
-    rate = node.create_rate(10, node.get_clock())
-    while img is None:
-        rate.sleep()
-
+    img = get_latest_msg_from_topic(
+        node,
+        "/zed/zed_node/left/image_rect_color",
+        Image,
+        callback_group,
+    )
+    node.logger.info("Received image.")
     return img
 
 
@@ -73,44 +49,68 @@ def get_zed_intrinsics(
                                     [0, 0, 1]]
             1x5 ndarray dist_coeffs: [k1, k2, p1, p2, k3]
     """
-    intrinsics_qos = QoSProfile(
-        depth=5,
-        history=QoSHistoryPolicy.KEEP_LAST,
-        reliability=QoSReliabilityPolicy.RELIABLE,
-        durability=QoSDurabilityPolicy.VOLATILE,
+    # Get CameraInfo message from topic
+    node.logger.info("Waiting for intrinsics...")
+    cam_info = get_latest_msg_from_topic(
+        node,
+        "/zed/zed_node/left/camera_info",
+        CameraInfo,
+        callback_group,
     )
+    node.logger.info("Received intrinsics.")
 
-    dist_coeffs = None
-    mtx = None
+    # Extract intrinsic parameters from CameraInfo message
+    fx, _, cx, _, fy, cy, *_ = iter(cam_info.k)
+    k1, k2, p1, p2, k3, *_ = iter(cam_info.d)
 
-    def intrinsics_callback(msg: CameraInfo) -> None:
-        nonlocal dist_coeffs, mtx
+    # Create the camera matrix mtx
+    mtx = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float64)
 
-        # Extract intrinsic parameters from CameraInfo message
-        fx, _, cx, _, fy, cy, *_ = iter(msg.k)
-        k1, k2, p1, p2, k3, *_ = iter(msg.d)
+    # Create a vector containing the known distortion coefficients
+    dist_coeffs = np.array([k1, k2, p1, p2, k3], dtype=np.float64)
 
-        # Create a vector containing the known distortion coefficients
-        dist_coeffs = np.array([k1, k2, p1, p2, k3], dtype=np.float64)
+    return mtx, dist_coeffs
 
-        # Create the camera matrix mtx
-        mtx = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float64)
 
-        node.logger.info("Received intrinsics.")
+AnyMessage = TypeVar("AnyMessage")
+
+
+def get_latest_msg_from_topic(
+    node: Node,
+    topic: str,
+    msg_type: type[AnyMessage],
+    callback_group: ReentrantCallbackGroup,
+) -> AnyMessage:
+    """
+    Return latest message from specified topic.
+
+    Args:
+        node: ROS2 node.
+        topic: Topic that the desired message is published to.
+        msg_type: Type of the desired message.
+        callback_group: Reentrant callback group.
+
+    Returns:
+        Raw message.
+    """
+    msg = None
+
+    def update_msg(incoming_msg: Image) -> None:
+        nonlocal msg
+        msg = incoming_msg
         node.destroy_subscription(subs)
 
     subs = node.create_subscription(
-        CameraInfo,
-        "/zed/zed_node/left/camera_info",
-        intrinsics_callback,
+        msg_type,
+        topic,
+        update_msg,
+        qos_profile=1,
         callback_group=callback_group,
-        qos_profile=intrinsics_qos,
     )
 
-    # Sleep until intrinsics can be retrieved
-    node.logger.info("Waiting for intrinsics...")
+    # Sleep until image can be retrieved to ensure image is there
     rate = node.create_rate(10, node.get_clock())
-    while mtx is None:
+    while msg is None:
         rate.sleep()
 
-    return mtx, dist_coeffs
+    return msg
