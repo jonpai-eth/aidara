1f9d360bbf75bb86feec197e0538db21a29d65d4
jonpai-eth <146820382+jonpai-eth@users.noreply.github.com>
2024-03-20 17:32:49 +0100

    Add transform point and retry behavior (#47)

--- code/tf2_service/tf2_service/tf2_server.py
+++ code/tf2_service/tf2_service/tf2_server.py
@@ -1,16 +1,30 @@
 """Node providing tf functionality via service interfaces."""
 
+from typing import TypeVar
+
 import rclpy
-from aidara_msgs.srv import Tf2DoTransform, Tf2GetTransform
+from aidara_msgs.srv import Tf2GetTransform, Tf2TransformPoint, Tf2TransformPose
 from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
 from rclpy.node import Node
 from rclpy.time import Duration, Time
 from tf2_geometry_msgs import (
+    PointStamped,  # noqa: F401. Required for buffer.transform.
     PoseStamped,  # noqa: F401. Required for buffer.transform.
 )
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
+
+TransformRequest = TypeVar(
+    "TransformRequest",
+    Tf2TransformPoint.Request,
+    Tf2TransformPose.Request,
+)
+TransformResponse = TypeVar(
+    "TransformResponse",
+    Tf2TransformPoint.Response,
+    Tf2TransformPose.Response,
+)
 
 
 class Tf2Server(Node):
@@ -29,9 +43,14 @@ class Tf2Server(Node):
             self._get_transform_cb,
         )
 
-        self._do_transform_srv = self.create_service(
-            Tf2DoTransform,
-            "~/do_transform",
+        self._transform_pose_srv = self.create_service(
+            Tf2TransformPose,
+            "~/transform_pose",
+            self._do_transform_cb,
+        )
+        self._transform_point_srv = self.create_service(
+            Tf2TransformPoint,
+            "~/transform_point",
             self._do_transform_cb,
         )
 
@@ -46,41 +65,48 @@ class Tf2Server(Node):
         target_frame = request.target_frame
         source_frame = request.source_frame
 
-        try:
-            res = self._tf_buffer.lookup_transform(
-                target_frame,
-                source_frame,
-                Time(),
-            )
-        except TransformException:
+        for _ in range(3):
+            try:
+                res = self._tf_buffer.lookup_transform(
+                    target_frame,
+                    source_frame,
+                    Time(),
+                    Duration(seconds=1),
+                )
+            except TransformException:
+                continue
+
+            response.tf = res
+            response.success = True
             return response
 
-        response.tf = res
-        response.success = True
         return response
 
     def _do_transform_cb(
         self,
-        request: Tf2DoTransform.Request,
-        response: Tf2DoTransform.Response,
-    ) -> Tf2DoTransform.Response:
-        """Transform pose from pose frame to target frame."""
+        request: TransformRequest,
+        response: TransformResponse,
+    ) -> TransformResponse:
+        """Transform transformable from original frame to target frame."""
         response.success = False
 
         target_frame = request.target_frame
-        source_pose = request.source_pose
+        source = request.source
 
-        try:
-            res = self._tf_buffer.transform(
-                source_pose,
-                target_frame,
-                timeout=Duration(seconds=1),
-            )
-        except TransformException:
+        for _ in range(3):
+            try:
+                res = self._tf_buffer.transform(
+                    source,
+                    target_frame,
+                    timeout=Duration(seconds=1),
+                )
+            except TransformException:
+                continue
+
+            response.result = res
+            response.success = True
             return response
 
-        response.pose = res
-        response.success = True
         return response
 
 
